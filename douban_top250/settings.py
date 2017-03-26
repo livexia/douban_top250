30d23fc40377f46a4e0944d51f1394d319c7e8e5
livexia <istillalive@live.cn>
2017-03-26 17:12:18 +0800

    增加截图，以及爬虫运行的gif

--- douban_top250/settings.py
+++ douban_top250/settings.py
@@ -91,6 +91,6 @@ ROBOTSTXT_OBEY = True
 
 # TODO
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
-ITEM_PIPELINES = {
-   'douban_top250.pipelines.DoubanTop250Pipeline': 300,
-}
+# ITEM_PIPELINES = {
+#    'douban_top250.pipelines.DoubanTop250Pipeline': 300,
+# }
